80b3b8344b161b64746c53f816bc8c82c15de622
unknown <ahmedwoye@gmail.com>
2023-04-30 17:57:56 +0100

    Testing the wigan app

---
+++ WiganApp/src/main/java/com/albiscomglobal/WiganApp/WiganAppApplication.java
@@ -0,0 +1,13 @@
+package com.albiscomglobal.WiganApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiganAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiganAppApplication.class, args);
+	}
+
+}
